766097536057d8825298d5bc206c67778cd3b65c
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 05:51:04 -0300

    movie serializer

---
+++ movie-app/app/serializers/movie.js
@@ -0,0 +1,10 @@
+import RESTSerializer  from '@ember-data/serializer/rest';
+
+export default class MovieSerializer extends RESTSerializer  {
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    // TMDB rest api returns data inside results:[] ; but Ember expects them at the root
+    payload = { movies: payload.results };
+    
+    return super.normalizeResponse(store, primaryModelClass, payload, id, requestType);
+  }
+}
