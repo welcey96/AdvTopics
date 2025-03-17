1b59a789b43b13ddb195c1527f79f7eb4b2d1fc4
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 05:05:51 -0300

    add movie model

---
+++ movie-app/app/models/movie.js
@@ -0,0 +1,18 @@
+import Model, { attr } from '@ember-data/model';
+import config from 'movie-app/config/environment';
+
+export default class MovieModel extends Model {
+  @attr title;
+  @attr poster_path;
+  @attr({ defaultValue: 'n/a' }) overview;
+  @attr backdrop_path;
+  @attr release_date;
+
+  get image() {
+    return `${config.TMDB_IMG}${this.poster_path}`;
+  }
+
+  get backdrop() {
+    return `${config.TMDB_IMG}${this.backdrop_path}`;
+  }
+}
