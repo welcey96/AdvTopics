e06547ca6485b5b57bdadd0a2fe178713a2c553b
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 05:56:17 -0300

    verify that movies is cached

--- movie-app/app/routes/movies/index.js
+++ movie-app/app/routes/movies/index.js
@@ -3,7 +3,18 @@ import { service } from '@ember/service';
 
 export default class MoviesIndexRoute extends Route {
   @service store;
+
   async model() {
-    return await this.store.findAll('movie');
+
+    let cachedMovies = await this.store.peekAll('movie');
+    console.log('🚀 ~ Cached movies:', cachedMovies.map((movie) => movie.serialize()));
+
+    if (!cachedMovies.length) {
+        let movies = await this.store.findAll('movie');
+        console.log('🚀 Movies Data:',movies.map((movie) => movie.serialize()));
+        return movies;
+    }
+
+    return cachedMovies;
   }
 }
