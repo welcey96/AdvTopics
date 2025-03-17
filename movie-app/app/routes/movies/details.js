3312816e7c50b300baa604f6686dacf0a6bc8b8d
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 03:31:02 -0300

    route redirection for /details if id is not valid

--- movie-app/app/routes/movies/details.js
+++ movie-app/app/routes/movies/details.js
@@ -1,8 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
-
 export default class MoviesDetailsRoute extends Route {
   @service router;
 
+  redirect(pathParams) {
+    console.log('🚀 ~ MoviesDetailsRoute ~ redirect ~ model:', pathParams.id);
+    if (isNaN(+pathParams.id)) this.router.transitionTo('movies');
+  }
 }
