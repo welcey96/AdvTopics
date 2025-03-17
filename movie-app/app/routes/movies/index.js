20436531cf5d566675af60bee1944399b8c199d8
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 05:51:35 -0300

    findAll movies

--- movie-app/app/routes/movies/index.js
+++ movie-app/app/routes/movies/index.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class MoviesIndexRoute extends Route {}
+export default class MoviesIndexRoute extends Route {
+  @service store;
+  async model() {
+    return await this.store.findAll('movie');
+  }
+}
