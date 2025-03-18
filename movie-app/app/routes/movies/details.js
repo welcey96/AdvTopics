84d90729b5651712c63ceb9501c6fd0f12fa4272
Mary Welcey Guillen <welceyg@gmail.com>
2025-03-17 21:20:26 -0300

    inject service to route

--- movie-app/app/routes/movies/details.js
+++ movie-app/app/routes/movies/details.js
@@ -3,6 +3,11 @@ import { service } from '@ember/service';
 
 export default class MoviesDetailsRoute extends Route {
   @service router;
+  @service movie;
+
+  async model(params) {
+    return await this.movie.get(params.id);
+  }
 
   redirect(pathParams) {
     console.log('🚀 ~ MoviesDetailsRoute ~ redirect ~ model:', pathParams.id);
